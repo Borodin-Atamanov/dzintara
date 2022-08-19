67abfbccc2f3baeba8321a0bf67042f117a6ed4e
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-08-19 18:59:54 +0400

    2022-08-19_18-59-54

--- tasks/add_screen_resolution_1280x1024_with_xrandr.sh
+++ tasks/add_screen_resolution_1280x1024_with_xrandr.sh
@@ -2,7 +2,7 @@
 #Author dev@Borodin-Atamanov.ru
 #License: MIT
 cvt 1280 1024 60
-modeline='Modeline "1280x1024_60.00"  109.00  1280 1368 1496 1712  1024 1027 1034 1063 -hsync +vsync';
+#modeline='Modeline "1280x1024_60.00"  109.00  1280 1368 1496 1712  1024 1027 1034 1063 -hsync +vsync';
 modeline_name='"1280x1024_60.00"';
 modeline="${modeline_name}  109.00  1280 1368 1496 1712  1024 1027 1034 1063 -hsync +vsync";
 echo $modeline;
