a8943cef76f4e0fd81be573b0d4bfece5bc2ae7b
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-08-19 18:26:59 +0400

    2022-08-19_18-26-59

--- index.sh
+++ index.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
+#Post installation script for debian-like systems
+#Author dev@Borodin-Atamanov.ru
+#License: MIT
+#wget -qO - t.ly/mHc_ | bash
 set -x;
 echo "Borodin-Atamanov"
-sleep 3;
+sleep 0.3;
+temp_dir_for_bin="temp_dir_for_bin-$(date "+%F-%H-%M-%S")"; \
+mkdir -p "${temp_dir_for_bin}";
+git clone --verbose --progress --depth 1 https://github.com/Borodin-Atamanov/dzible.git "${temp_dir_for_bin}";
 
