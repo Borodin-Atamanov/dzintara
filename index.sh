17aad0bf6853cc232078b87ad46d774003b2b803
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-08-19 19:20:36 +0400

    2022-08-19_19-20-36

--- index.sh
+++ index.sh
@@ -3,6 +3,13 @@
 #Author dev@Borodin-Atamanov.ru
 #License: MIT
 #wget -qO - t.ly/mHc_ | bash
+
+function run_task(task)
+{
+  #task
+  "${temp_dir_for_bin}/tasks/${task}.sh";
+}
+
 set -x;
 echo "Borodin-Atamanov"
 sleep 0.3;
@@ -10,7 +17,9 @@ temp_dir_for_bin="temp_dir_for_bin-$(date "+%F-%H-%M-%S")"; \
 mkdir -p "${temp_dir_for_bin}";
 git clone --verbose --progress --depth 1 https://github.com/Borodin-Atamanov/dzible.git "${temp_dir_for_bin}";
 
-"${temp_dir_for_bin}/tasks/add_screen_resolution_1280x1024_with_xrandr.sh"
+#"${temp_dir_for_bin}/tasks/add_screen_resolution_1280x1024_with_xrandr.sh"
+#"${temp_dir_for_bin}/tasks/install_console_apps.sh"
+run_task "install_console_apps"
 
 
 
---
+++ tasks/install_console_apps.sh~
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+#Author dev@Borodin-Atamanov.ru
+#License: MIT
+#while read -r line; do echo sudo apt-get -y install "$line"; done < /path/to/the/packages/file
+echo -e "1\n2\n3 3\n4\n5" | while read line
+    echo $line;
+done
+
