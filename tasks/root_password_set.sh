3c3270727dc05c74e88aa4470006f7634db0c234
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-08-23 21:43:26 +0400

    2022-08-23_21-43-26

--- tasks/root_password_set.sh
+++ tasks/root_password_set.sh
@@ -11,9 +11,15 @@ fi;
 var_name="${secrets}_${computer_name}_root_pass";
 show_var "$var_name"
 
+
+
+
 if [[ $EUID -ne 0 ]]; then
    echo "Must be run as root! $0"
    exit 1
 fi
+
+#echo -e "$password\n$password\n" | sudo passwd root
+echo -e "$( get_var "${var_name}" )\n$( get_var "${var_name}" )" | passwd root
 
 passwd --status  --all
