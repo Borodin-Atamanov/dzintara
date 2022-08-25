3a5b9467b7419ce8f8032be547796fb4ed7d245c
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-08-25 12:25:15 +0400

    2022-08-25_12-25-15

--- push2github.sh
+++ push2github.sh
@@ -4,12 +4,12 @@ set -x;
 ./encrypt_all_vaults.sh
 sleep $sleep_time;
 time ( \
-git diff
-sleep $sleep_time;
-echo git pull --verbose;
-git whatchanged | head --lines=42
-sleep $sleep_time;
-git add --verbose --all;
-sleep $sleep_time;
-git commit --allow-empty-message --message=$(date "+%F_%H-%M-%S") --verbose; git push --verbose;
+    git diff
+    sleep $sleep_time;
+    echo git pull --verbose;
+    git whatchanged | head --lines=42
+    sleep $sleep_time;
+    git add --verbose --all;
+    sleep $sleep_time;
+    git commit --allow-empty-message --message=$(date "+%F_%H-%M-%S") --verbose; git push --verbose;
 )
