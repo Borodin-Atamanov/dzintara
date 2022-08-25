91e008c91a272d1c8f32dcc1f40d0a4ba596cafb
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-08-25 15:03:34 +0400

    2022-08-25_15-03-34

---
+++ autorun/load_variables.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+#Author dev@Borodin-Atamanov.ru
+#License: MIT
+#This file contains automaticaly generated variables, what using in target system in every run
+
---
+++ autorun/user_autorun.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+#Post installation script for debian-like systems
+#Author dev@Borodin-Atamanov.ru
+#License: MIT
+#script autorun on target system in user mode
+
