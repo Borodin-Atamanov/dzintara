3d746019c2339c9b52db1b032bf412c1757d4eb7
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-08-26 16:26:56 +0400

    2022-08-26_16-26-56

--- index.sh
+++ index.sh
@@ -349,7 +349,6 @@ fi
 # run_task root_password_for_sudoers
 run_task install_autorun_script
 
-exit 0;
 
 else
     echo 'functions loaded';
@@ -357,4 +356,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-script_subversion='uvune-dbc7efb-2022-08-26-16-26-01'; echo "${script_subversion}=script_subversion"; 
+script_subversion='dufax-51b2614-2022-08-26-16-26-55'; echo "${script_subversion}=script_subversion"; 
