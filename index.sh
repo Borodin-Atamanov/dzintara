6e5224c1453065823bcd4c1800cb9cea15f2bae6
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-08-26 23:48:05 +0400

    2022-08-26_23-48-05

--- index.sh
+++ index.sh
@@ -360,4 +360,4 @@ fi; #end of fun if
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
 export \
-script_subversion='bubom-0fc216c-2022-08-26-23-42-22'; echo "${script_subversion}=script_subversion"; 
+script_subversion='feren-9376967-2022-08-26-23-48-04'; echo "${script_subversion}=script_subversion"; 
