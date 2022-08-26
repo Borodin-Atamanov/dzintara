51b2614acb40bc55f1d736bb732cf8e0292c49dc
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-08-26 16:26:02 +0400

    2022-08-26_16-26-02

--- index.sh
+++ index.sh
@@ -357,4 +357,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-script_subversion='nomot-3863614-2022-08-26-16-20-02'; echo "${script_subversion}=script_subversion"; 
+script_subversion='uvune-dbc7efb-2022-08-26-16-26-01'; echo "${script_subversion}=script_subversion"; 
