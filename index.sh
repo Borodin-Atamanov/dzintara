b692c126008b38b157d303d47edd1b69ed6c1ca3
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-08-26 16:34:34 +0400

    2022-08-26_16-34-34

--- index.sh
+++ index.sh
@@ -357,3 +357,4 @@ fi; #end of fun if
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
 export script_subversion='osito-4b30c71-2022-08-26-16-34-29'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='oxuva-6f13eda-2022-08-26-16-34-33'; echo "${script_subversion}=script_subversion"; 
