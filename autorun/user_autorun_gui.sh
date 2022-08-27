07751d91f054aa2b9a6a4806c1ec7a70beff2cc4
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-08-27 20:12:47 +0400

    fakep-e4e86e7-2022-08-27-20-12-46

--- autorun/user_autorun_gui.sh
+++ autorun/user_autorun_gui.sh
@@ -14,12 +14,15 @@ declare_and_export work_dir "/home/i/bin/dzible/"
 
 echo "$work_dir";
 
-xset led 3;
+#xset led 3;
 
-cvt_xrandr 1280 1024 60
+run_task show_script_subversion
+run_task add_screen_resolution_with_cvt_xrandr
+
+#cvt_xrandr 1280 1024 60
 
 #cvt_xrandr 1920 1080 60
-cvt_xrandr 1360 768 60
+#cvt_xrandr 1360 768 60
 
 for ((i=142;i>=0;i--)); do echo -ne "\b\b\b\b\b\b\b\b $i  "; sleep 0.42; done;
 
