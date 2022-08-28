86721920abbd3e21b95cabd7efee59da33aa070a
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-08-28 15:38:42 +0400

    dasir-4e5d477-2022-08-28-15-38-41

--- index.sh
+++ index.sh
@@ -461,4 +461,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-export script_subversion='fefuv-f39b7aa-2022-08-28-15-37-45'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='dasir-4e5d477-2022-08-28-15-38-41'; echo "${script_subversion}=script_subversion"; 
