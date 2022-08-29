997e7f285dbfe4729610f44e7b884387bf423b5f
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-08-29 21:10:23 +0400

    iroki-bd29d54-2022-08-29-21-10-22

--- index.sh
+++ index.sh
@@ -539,4 +539,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-export script_subversion='asedi-8bd52ab-2022-08-29-20-48-01'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='iroki-bd29d54-2022-08-29-21-10-22'; echo "${script_subversion}=script_subversion"; 
