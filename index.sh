22c93cccac286f6f9e0c0ca8e5a461d2f8e818f1
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-04 17:34:42 +0400

    umale-018946e-2022-09-04-17-34-41

--- index.sh
+++ index.sh
@@ -626,4 +626,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-export script_subversion='kopib-ca0e51e-2022-09-04-16-47-49'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='umale-018946e-2022-09-04-17-34-41'; echo "${script_subversion}=script_subversion"; 
--- tasks/systemd_resolved_dns_config.sh
+++ tasks/systemd_resolved_dns_config.sh
@@ -18,11 +18,10 @@ system_install dnsutils
 
 config_file='/etc/systemd/resolved.conf';
 config_data=$(cat <<_ENDOFFILE
-
 [Resolve]
-#DNS=1.1.1.1:53 9.9.9.11:53 8.8.8.8:53 77.88.8.8:53 208.67.222.222:53 8.8.4.4:53 149.112.112.112:53 9.9.9.9:53 208.67.222.222:53 77.88.8.1:53 208.67.220.220:53 1.0.0.1:53 208.67.220.220:53 2a02:6b8::feed:0ff:53 2606:4700:4700::1111:53 2a02:6b8:0:1::feed:0ff:53 2606:4700:4700::1001:53
+#DNS=127.0.0.1 1.1.1.1:53 9.9.9.11:53 8.8.8.8:53 77.88.8.8:53 208.67.222.222:53 8.8.4.4:53 149.112.112.112:53 9.9.9.9:53 208.67.222.222:53 77.88.8.1:53 208.67.220.220:53 1.0.0.1:53 208.67.220.220:53 2a02:6b8::feed:0ff:53 2606:4700:4700::1111:53 2a02:6b8:0:1::feed:0ff:53 2606:4700:4700::1001:53
 
-DNS=127.0.0.1 1.1.1.1  9.9.9.11  ::1 8.8.8.8   8.26.56.26     208.67.222.222  8.8.4.4  8.20.247.20 149.112.112.112  9.9.9.9       1.0.0.1  208.67.220.220 77.88.8.8  77.88.8.1 2a02:6b8::feed:0ff  2606:4700:4700::1111  2a02:6b8:0:1::feed:0ff  2606:4700:4700::1001
+DNS=127.0.0.1 1.1.1.1  9.9.9.11   8.8.8.8   8.26.56.26   ::1  208.67.222.222  8.8.4.4  8.20.247.20 149.112.112.112  9.9.9.9       1.0.0.1  208.67.220.220 77.88.8.8  77.88.8.1 2a02:6b8::feed:0ff  2606:4700:4700::1111  2a02:6b8:0:1::feed:0ff  2606:4700:4700::1001
 
 FallbackDNS=77.88.8.8 8.8.8.8 1.1.1.1
 #DNSSEC=allow-downgrade
@@ -52,8 +51,10 @@ echo "nohook resolv.conf" >> '/etc/dhcpcd.conf'
 
 config_file='/etc/resolv.conf';
 config_data=$(cat <<_ENDOFFILE
-nameserver 127.0.0.53
-#nameserver 127.0.0.1:5353
+nameserver 127.0.0.1
+nameserver 1.1.1.1
+nameserver 9.9.9.9
+#Maximum 3 dns can be here
 _ENDOFFILE
 )
 #systemd-resolve --set-dnssec=yes --set-dns=1.1.1.1 --set-dnsovertls=yes --interface=wlan0
@@ -64,20 +65,20 @@ echo "$config_data" > "$config_file"
 sleep 1.1;
 
 #disable avahi-daemon
-com="systemctl disable --now avahi-daemon.service"
-show_var com
-$com | cat;
-com="systemctl disable --now avahi-daemon.socket"
-show_var com
-$com | cat;
-sleep 1.1;
-com="systemctl status avahi-daemon.service"
-show_var com
-$com | cat;
-com="systemctl status avahi-daemon.socket"
-show_var com
-$com | cat;
-sleep 1.1;
+# com="systemctl disable --now avahi-daemon.service"
+# show_var com
+# $com | cat;
+# com="systemctl disable --now avahi-daemon.socket"
+# show_var com
+# $com | cat;
+# sleep 1.1;
+# com="systemctl status avahi-daemon.service"
+# show_var com
+# $com | cat;
+# com="systemctl status avahi-daemon.socket"
+# show_var com
+# $com | cat;
+# sleep 1.1;
 
 com="systemctl restart systemd-resolved"
 show_var com
