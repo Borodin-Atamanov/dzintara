908d27f63c8590c2bf8d5ef50e9c6e8e6d7d0ec5
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-04 14:48:52 +0400

    zidul-ecbfdca-2022-09-04-14-48-51

--- index.sh
+++ index.sh
@@ -618,4 +618,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-export script_subversion='ekoga-eccf907-2022-09-04-14-48-34'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='zidul-ecbfdca-2022-09-04-14-48-51'; echo "${script_subversion}=script_subversion"; 
