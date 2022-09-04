31ca01fe8ceeb673532899fee0cd7b062db2950a
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-04 14:02:45 +0400

    mogir-2039d47-2022-09-04-14-02-44

--- index.sh
+++ index.sh
@@ -617,4 +617,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-export script_subversion='xinof-8e61995-2022-09-04-13-22-03'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='mogir-2039d47-2022-09-04-14-02-44'; echo "${script_subversion}=script_subversion"; 
