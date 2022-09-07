c45985e535c8a6bbcb553c774eaf9709584933fb
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-07 16:04:44 +0400

    dobor-864bc27-2022-09-07-16-04-43

--- index.sh
+++ index.sh
@@ -649,4 +649,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-export script_subversion='bezon-47b633a-2022-09-07-16-02-57'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='dobor-864bc27-2022-09-07-16-04-43'; echo "${script_subversion}=script_subversion"; 
