e0f53c9a96ad55999f84a0df516ada1756979b49
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-09 23:27:20 +0400

    badag-229bed4-2022-09-09-23-27-19

--- index.sh
+++ index.sh
@@ -788,4 +788,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-export script_subversion='ukeme-2df91eb-2022-09-09-23-24-39'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='badag-229bed4-2022-09-09-23-27-19'; echo "${script_subversion}=script_subversion"; 
