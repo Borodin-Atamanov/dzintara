18f2972cca658b35a3a7a4ccbce770a829392811
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-09 23:41:38 +0400

    bamol-21cc65e-2022-09-09-23-41-37

--- autorun/xkeyboard_autorun.sh
+++ autorun/xkeyboard_autorun.sh
@@ -19,28 +19,32 @@ declare -x -g service_name='dzible.xkeyboard';   #for slog systemd logs
 whoami="$(whoami)"
 slog "<7>start X11 input settings update service"
 
-numlockx_fp="$( get_command_fullpath numlockx )";
-xset_fp="$( get_command_fullpath xset )";
-setxkbmap_fp="$( get_command_fullpath setxkbmap )";
-
-gxkb_fp="$( get_command_fullpath gxkb )";
-
-nohup gxkb_fp &
+numlockx="$( get_command_fullpath numlockx )";
+xset="$( get_command_fullpath xset )";
+setxkbmap="$( get_command_fullpath setxkbmap )";
+gxkb="$( get_command_fullpath gxkb )";
+xneur="$( get_command_fullpath xneur )";
 
 #echo -e "remove Lock = Caps_Lock\nremove Control = Control_L\nkeysym Control_L = Caps_Lock\nkeysym Caps_Lock = Control_L\nadd Lock = Caps_Lock\nadd Control = Control_L\n" | xmodmap -v -
+#timeout --kill-after=$timeout_1 $timeout_2 $setxkbmap -layout "us,ru" -option "" -option "grp:shift_caps_switch" -option "grp_led:scroll" -option "grp_led:caps" -option "compose:sclk"
 
-while : ; do :
-    timeout --kill-after=$timeout_1 $timeout_2 $numlockx_fp on
-    timeout --kill-after=$timeout_1 $timeout_2 $xset_fp led 3;
-    #timeout --kill-after=$timeout_1 $timeout_2 $setxkbmap_fp -layout "us,ru"
+#while : ; do :
+for ((i=7;i>=0;i--)); do
+    #timeout --kill-after=$timeout_1 $timeout_2 $numlockx on
+    $numlockx on
+    $xset led 3;
+    nohup $setxkbmap -layout "us,ru" -option "" -option "grp:shift_caps_switch" -option "grp_led:scroll" -option "grp_led:caps" -option "compose:sclk" &
+    nohup gxkb &
+    #timeout --kill-after=$timeout_1 $timeout_2 $xset led 3;
+    #timeout --kill-after=$timeout_1 $timeout_2 $setxkbmap -layout "us,ru"
     # https://gist.github.com/jatcwang/ae3b7019f219b8cdc6798329108c9aee
     #lv3:enter_switch
-    timeout --kill-after=$timeout_1 $timeout_2 $setxkbmap_fp -layout "us,ru" -option "" -option "grp:shift_caps_switch" -option "grp_led:scroll" -option "grp_led:caps" -option "compose:sclk"
     #TODO turn off caps lock (if it accidentally turned on)
     #
     sleep $timeout_2
-    #timeout --kill-after=$timeout_1 $timeout_2 $xset_fp -led 3;
+    #timeout --kill-after=$timeout_1 $timeout_2 $xset -led 3;
     #sleep "1.$RANDOM"
     #sleep $timeout_1
 done;
 
+nohup $xneur &
