6fed6b95f7ca27b7fdcdfa709fc6735f251cb702
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-09 21:17:03 +0400

    uruta-bf0799d-2022-09-09-21-17-02

--- index.sh
+++ index.sh
@@ -777,4 +777,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-export script_subversion='kivot-5276971-2022-09-09-20-11-33'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='uruta-bf0799d-2022-09-09-21-17-02'; echo "${script_subversion}=script_subversion"; 
