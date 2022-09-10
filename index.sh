c5860c32b1191c4ed666b0c14be54cc67d3befcf
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-10 22:32:26 +0400

    memab-a8d4195-2022-09-10-22-32-25

--- index.sh
+++ index.sh
@@ -832,4 +832,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-export script_subversion='otate-a98514e-2022-09-10-22-00-55'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='memab-a8d4195-2022-09-10-22-32-25'; echo "${script_subversion}=script_subversion"; 
