f86e133a5e97332083e6a29314739e89dfed3c04
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-10 19:25:57 +0400

    solog-5ed8ec1-2022-09-10-19-25-56

--- index.sh
+++ index.sh
@@ -820,4 +820,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-export script_subversion='roxav-494f403-2022-09-10-19-18-29'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='solog-5ed8ec1-2022-09-10-19-25-56'; echo "${script_subversion}=script_subversion"; 
