5ed8ec14aaabc923ac25308f99700dc3f590cd22
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-10 19:18:30 +0400

    roxav-494f403-2022-09-10-19-18-29

--- index.sh
+++ index.sh
@@ -820,4 +820,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-export script_subversion='oxaga-c0f86ac-2022-09-10-19-17-36'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='roxav-494f403-2022-09-10-19-18-29'; echo "${script_subversion}=script_subversion"; 
