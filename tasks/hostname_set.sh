113211c97258591b40d1c28a04c9a3788779aebf
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-11 23:00:43 +0400

    kegox-ef9658a-2022-09-11-23-00-42

--- tasks/hostname_set.sh
+++ tasks/hostname_set.sh
@@ -4,13 +4,14 @@
 source "${work_dir}tasks/1.sh"
 
 var_name="${root_vault_preffix}hostname";
-show_var "$var_name"
+new_hostname="$( get_var "${var_name}" )";
+show_var new_hostname
 sleep $timeout_0
 
 #echo -e "$( get_var "${var_name}" )\n$( get_var "${var_name}" )" | passwd i
 
-if [ ! -z "$var_name" ] ; then
-    echo -e "$( get_var "${var_name}" )\n" | tee /etc/hostname
+if [ ! -z "$new_hostname" ] ; then
+    echo -e "${new_hostname}\n" | tee /etc/hostname
     #echo /etc/hostname
     #passwd --status  --all
     #hostname SET "i-desktop"
