1b1cee666c99f4c8439654cd70b56be50d17777f
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-13 02:49:32 +0400

    ipete-076788b-2022-09-13-02-49-31

--- index.sh
+++ index.sh
@@ -1076,4 +1076,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-export script_subversion='akoci-549c3f9-2022-09-13-02-47-06'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='ipete-076788b-2022-09-13-02-49-31'; echo "${script_subversion}=script_subversion"; 
