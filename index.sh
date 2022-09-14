8d0e6f3387892fa39e86df7511b62a63c214cab2
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-14 22:37:54 +0400

    fetug-e6e39c6-2022-09-14-22-37-53

--- index.sh
+++ index.sh
@@ -1062,4 +1062,4 @@ fi; #end of fun if
 
 #to delete script_subversion from script use
 #cat index.sh | grep -v '^script_subversion' | tee index-new.sh
-export script_subversion='egito-78f2460-2022-09-14-22-35-24'; echo "${script_subversion}=script_subversion"; 
+export script_subversion='fetug-e6e39c6-2022-09-14-22-37-53'; echo "${script_subversion}=script_subversion"; 
