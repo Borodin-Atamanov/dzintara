fa6b6c0fe8d0c6079fc74715c3ad2cfd010dc16a
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-16 17:07:16 +0400

    osugua-164-2209161707

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='ofupuu-163-2209161703'; 
+declare -g -x script_version='osugua-164-2209161707'; 
 
 function run_task ()
 {
