1dbee5b080f610dccd4ea3808cb751d09c80320a
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-16 15:31:08 +0400

    sudaxn-156-2209161531

--- tasks/passwords_to_user_show.sh
+++ tasks/passwords_to_user_show.sh
@@ -49,11 +49,129 @@ remove_root_vault_preffix www_user
 remove_root_vault_preffix www_password
 remove_root_vault_preffix vnc_password
 
-get_all_host_addresses all_ip
-echo "$all_ip"
+get_all_host_addresses all_addrs
+echo "$all_addrs"
 sleep 1;
 
-declare -g -x user_text=$(cat <<_ENDOFFILE
+user_text='';
+
+user_text=$(cat <<_ENDOFFILE
+${user_text}
+_ENDOFFILE
+)
+
+user_text=$(cat <<_ENDOFFILE
+${user_text}
+# Save this important information about your system passwords in your password manager
+# This fille will delete after system reboot
+
+passwor for system user 'i':
+${user_i_password}
+
+passwor for root super user:
+${user_i_password}
+
+
+_ENDOFFILE
+)
+
+user_text=$(cat <<_ENDOFFILE
+${user_text}
+
+# SSH access
+_ENDOFFILE
+)
+for this_addr in $all_addrs; do
+user_text=$(cat <<_ENDOFFILE
+${user_text}
+sshpass -p ${root_password} ssh -p 30222 root@${this_addr}
+sshpass -p ${user_i_password} ssh -p 30222 i@${this_addr}
+_ENDOFFILE
+)
+done;
+
+user_text=$(cat <<_ENDOFFILE
+${user_text}
+
+# SFTP access
+_ENDOFFILE
+)
+for this_addr in $all_addrs; do
+user_text=$(cat <<_ENDOFFILE
+${user_text}
+sftp://root:${root_password}@${this_addr}:30222/
+sftp://i:${user_i_password}@${this_addr}:30222/home/i/
+_ENDOFFILE
+)
+done;
+
+user_text=$(cat <<_ENDOFFILE
+${user_text}
+
+# Web access to root filesystem [ IPv6 should be in square brackets ]
+_ENDOFFILE
+)
+for this_addr in $all_addrs; do
+user_text=$(cat <<_ENDOFFILE
+${user_text}
+https://${www_user}:${www_password}@${this_addr}
+http://${this_addr}/home/i/share/
+_ENDOFFILE
+)
+done;
+
+user_text=$(cat <<_ENDOFFILE
+${user_text}
+
+# VNC access
+_ENDOFFILE
+)
+for this_addr in $all_addrs; do
+user_text=$(cat <<_ENDOFFILE
+${user_text}
+vnc://${vnc_password}@${this_addr}
+_ENDOFFILE
+)
+done;
+
+user_text=$(cat <<_ENDOFFILE
+${user_text}
+
+# Webmin access. Use system passwords
+_ENDOFFILE
+)
+for this_addr in $all_addrs; do
+user_text=$(cat <<_ENDOFFILE
+${user_text}
+https://${this_addr}:${webmin_port}
+_ENDOFFILE
+)
+done;
+
+
+all_ip_text=''
+for this_addr in $all_addrs; do
+ip_text=$(cat <<_ENDOFFILE
+this_addr=${this_addr}
+# SSH access
+sshpass -p ${root_password} ssh -p 30222 root@${this_addr}
+sshpass -p ${user_i_password} ssh -p 30222 i@${this_addr}
+
+# SFTP access
+sftp://root:${root_password}@${this_addr}:30222/
+sftp://i:${user_i_password}@${this_addr}:30222/home/i/
+
+# Web access to root filesystem [ IPv6 should be in square brackets ]
+https://${www_user}:${www_password}@${this_addr}
+
+_ENDOFFILE
+)
+all_ip_text="${all_ip_text}${ip_text}${x0a}"
+done;
+
+echo "$all_ip_text"
+
+user_text2=$(cat <<_ENDOFFILE
 # hostname
 ${hostname}
 
@@ -73,4 +191,7 @@ ${vnc_password}
 _ENDOFFILE
 )
 
-echo "$user_text"
+echo "$user_text" > "$password_in_plain_text_for_user_file"
+chown --verbose --changes i:i "$password_in_plain_text_for_user_file";
+chmod --verbose 0666 "$password_in_plain_text_for_user_file";
+
