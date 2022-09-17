42cfa73905ec9b2d38c00c503d9fb31b84a740f4
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-17 23:25:18 +0400

    esimem-191-2209172325

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='arezuu-190-2209172300'; 
+declare -g -x script_version='esimem-191-2209172325'; 
 
 function run_task ()
 {
