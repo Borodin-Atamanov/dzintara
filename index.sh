f850f62c206e912f8093b6093f7a60661284886f
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-17 23:58:08 +0400

    itedoc-779-2209172358

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='alidos-778-2209172345'; 
+declare -g -x script_version='itedoc-779-2209172358'; 
 
 function run_task ()
 {
