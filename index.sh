d8aad4d9cd0d5ef4414c95508fbfaf3807e7fd94
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-17 23:45:48 +0400

    alidos-778-2209172345

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='uvumea-777-2209172328'; 
+declare -g -x script_version='alidos-778-2209172345'; 
 
 function run_task ()
 {
