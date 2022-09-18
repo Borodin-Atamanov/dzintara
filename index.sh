ee4f3a772e7f68934fd23604572940d110593e2a
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-18 05:34:13 +0400

    kuzevk-794-2209180534

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='vibafe-793-2209180529'; 
+declare -g -x script_version='kuzevk-794-2209180534'; 
 
 function run_task ()
 {
