e886e383247d1bf10cf7a71481cfcfe5578bf8c5
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-18 04:44:04 +0400

    icideu-788-2209180444

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='icukii-787-2209180437'; 
+declare -g -x script_version='icideu-788-2209180444'; 
 
 function run_task ()
 {
