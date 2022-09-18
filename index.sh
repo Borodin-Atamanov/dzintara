11ba96a2b4b1b09ba9307ace02de0ad405edc4f4
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-18 17:20:12 +0400

    ebudao-804-2209181720

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='kozixx-803-2209181719'; 
+declare -g -x script_version='ebudao-804-2209181720'; 
 
 function run_task ()
 {
