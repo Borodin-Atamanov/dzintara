66d6b4d3a27059e171dadb1eadf804a25f4a4961
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-18 17:19:18 +0400

    kozixx-803-2209181719

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='pagepu-802-2209181718'; 
+declare -g -x script_version='kozixx-803-2209181719'; 
 
 function run_task ()
 {
