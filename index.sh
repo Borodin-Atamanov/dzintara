af065932301c96492c30061e51a0fea2c580f466
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-18 05:36:09 +0400

    veturi-795-2209180536

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='kuzevk-794-2209180534'; 
+declare -g -x script_version='veturi-795-2209180536'; 
 
 function run_task ()
 {
@@ -499,7 +499,7 @@ export -f is_process_running
 
 function ymdhms ()
 {
-  echo -n "$(date "+%F-%H-%M-%S-%N")";
+  echo -n "$(date "+%F-%H-%M-%S")";
 }
 export -f ymdhms
 
