c6c7e899a27c6d55d4610a8ed3b9a4d9a71936af
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-18 18:27:38 +0400

    sixada-809-2209181827

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='avasak-808-2209181824'; 
+declare -g -x script_version='sixada-809-2209181827'; 
 
 function run_task ()
 {
