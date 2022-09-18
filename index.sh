0f911cd7bcfe939b6bdc36578d336ae6bebd893c
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-18 17:21:23 +0400

    podori-805-2209181721

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='ebudao-804-2209181720'; 
+declare -g -x script_version='podori-805-2209181721'; 
 
 function run_task ()
 {
