8db9d2ac8dca02d352439b7041d0d24a98ce8c2f
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-22 00:34:36 +0400

    unaboo-854-2209220034

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='fudiri-853-2209220032'; 
+declare -g -x script_version='unaboo-854-2209220034'; 
 
 function run_task ()
 {
