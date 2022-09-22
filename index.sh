6df4b9dc3038bef8c50930c81c109bf5a1a642b8
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-22 21:37:12 +0400

    ileduk-863-2209222137

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='igapau-862-2209222123'; 
+declare -g -x script_version='ileduk-863-2209222137'; 
 
 function run_task ()
 {
