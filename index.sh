e6536250af6f6173c1a72afd375af94d80c1bfbe
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-22 22:30:37 +0400

    tipava-869-2209222230

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='pivulo-868-2209222209'; 
+declare -g -x script_version='tipava-869-2209222230'; 
 
 function run_task ()
 {
@@ -1176,7 +1176,7 @@ if [[ "$1" != "fun" ]]; then
 
 # copy output to log file
 start_log
-index_log=log_file
+index_log="$log_file"
 
 #
 # load variables from root_vault_file
