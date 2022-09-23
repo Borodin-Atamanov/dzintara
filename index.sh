075ae668668ecfe76d4e3af86c47a69232777f0f
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-23 17:13:04 +0400

    vexapn-877-2209231713

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='akudio-876-2209231654'; 
+declare -g -x script_version='vexapn-877-2209231713'; 
 
 function run_task ()
 {
