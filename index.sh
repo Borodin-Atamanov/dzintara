cb0bd2da63df31c31ef57081b9c6a9bda567f67f
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-24 11:32:20 +0400

    ebasiv-891-2209241132

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='ikugau-890-2209241116'; 
+declare -g -x script_version='ebasiv-891-2209241132'; 
 
 function run_task ()
 {
