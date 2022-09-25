eb20bde4efa01c3f0ef5d07c7371c5ee56161f87
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-25 22:39:50 +0400

    ozitae-908-2209252239

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='nacive-907-2209252213'; 
+declare -g -x script_version='ozitae-908-2209252239'; 
 
 function run_task ()
 {
