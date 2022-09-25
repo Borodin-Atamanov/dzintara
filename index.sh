afb5be082548343a61a547badc28113e4084b453
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-26 00:00:17 +0400

    xelilb-909-2209260000

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='ozitae-908-2209252239'; 
+declare -g -x script_version='xelilb-909-2209260000'; 
 
 function run_task ()
 {
