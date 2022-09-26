f19428be1fca9ed6a266d785cc8ea94ed588a902
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-26 23:34:51 +0400

    ravufc-915-2209262334

---
+++ app/cvt_xrandr_1280x1024_60.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#Post installation script for debian-like systems
+#Author dev@Borodin-Atamanov.ru
+#License: MIT
+# script try to start keeweb application in some way
+
+
+source "/home/i/bin/dzintara/autorun/load_variables.sh" nocd
+#declare_and_export work_dir "/home/i/bin/dzintara/"
+
+cvt_xrandr 1280 1024 60
