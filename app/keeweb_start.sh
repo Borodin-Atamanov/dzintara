52700b1c94941e53a1b079b95b0dcb7609b68d26
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-26 23:50:04 +0400

    berava-916-2209262350

--- app/keeweb_start.sh
+++ app/keeweb_start.sh
@@ -10,6 +10,12 @@ source "/home/i/bin/dzintara/autorun/load_variables.sh" nocd
 
 url="https://app.keeweb.info/?from=dzintara"
 
+command="qutebrowser"
+if [ -x "$(command -v "$command")" ]; then
+    $command --target tab "$url"
+    echo "ok: $command"; sleep 1; exit;
+fi
+
 command="falkon"
 if [ -x "$(command -v "$command")" ]; then
     $command --current-tab "$url"
