adb92088df2742fde2124754d6a28e80463ec6da
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-09-29 17:49:19 +0400

    inetos-938-2209291749

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='imoxoo-937-2209291748'; 
+declare -g -x script_version='inetos-938-2209291749'; 
 
 function run_task ()
 {
