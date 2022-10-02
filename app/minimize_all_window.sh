6e5fb66f6d1c9f00cc9b74ad43b014c1bdfe5a34
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-10-02 20:37:16 +0400

    agiguo-949-2210022037

--- app/minimize_all_window.sh
+++ app/minimize_all_window.sh
@@ -7,4 +7,5 @@
 
 source "/home/i/bin/dzintara/autorun/load_variables.sh"
 #declare_and_export work_dir "/home/i/bin/dzintara/"
+# TODO loop while 1200 milliseconds
 for i in {1..50}; do WID=$(xdotool getactivewindow); echo $WID; xdotool windowminimize $WID; sleep 0.015; done;
