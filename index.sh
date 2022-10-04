f3b6ada679c352c07d9b750c003995355cecabcf
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-10-04 16:09:09 +0400

    mifubv-1541-2210041609

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='lecemz-957-2210022244'; 
+declare -g -x script_version='mifubv-1541-2210041609'; 
 
 function run_task ()
 {
