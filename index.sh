1d2ae07b33836458704673d6bbf06227cb2766b0
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-10-04 23:07:43 +0400

    ritelz-961-2210042307

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='loduss-960-2210042304'; 
+declare -g -x script_version='ritelz-961-2210042307'; 
 
 function run_task ()
 {
