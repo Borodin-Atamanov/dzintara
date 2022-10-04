1c8b8b98bdeb2349d1c1e87db78ad5cf4b79c3ee
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-10-05 01:11:57 +0400

    iforat-969-2210050111

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='odiven-968-2210050105'; 
+declare -g -x script_version='iforat-969-2210050111'; 
 
 function run_task ()
 {
