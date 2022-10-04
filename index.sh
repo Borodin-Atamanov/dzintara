b61c72d0701298d74b1aebbc3a71e075aa3d5ce4
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-10-05 00:30:42 +0400

    raxuxf-965-2210050030

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='ugipeu-964-2210050023'; 
+declare -g -x script_version='raxuxf-965-2210050030'; 
 
 function run_task ()
 {
