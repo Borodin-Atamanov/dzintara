0fc5b61286cfa3de4e613be2c8da8d8770f56ad1
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-10-05 14:55:51 +0400

    ezoxao-975-2210051455

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='loline-974-2210051430'; 
+declare -g -x script_version='ezoxao-975-2210051455'; 
 
 function run_task ()
 {
