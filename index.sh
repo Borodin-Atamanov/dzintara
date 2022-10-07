5349cc6a325ea4be60e0add3b087bee6368e114e
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-10-07 17:13:48 +0400

    tulonz-995-2210071713

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='umipae-994-2210071705'; 
+declare -g -x script_version='tulonz-995-2210071713'; 
 
 function run_task ()
 {
