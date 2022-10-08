9c75eb672cf5ff890fe3501822efbd9f05390c7d
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-10-08 12:06:37 +0400

    cemeze-1002-2210081206

--- index.sh
+++ index.sh
@@ -18,7 +18,7 @@
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 "
 # ./index.sh tasks="install_autorun_script install_telemetry countdown:150:0.1 show_script_subversion:arg1:arg2 install_nginx_root"
 
-declare -g -x script_version='fugadi-1001-2210081159'; 
+declare -g -x script_version='cemeze-1002-2210081206'; 
 
 function run_task ()
 {
--- tasks/firefox_setup.sh
+++ tasks/firefox_setup.sh
@@ -2,28 +2,28 @@
 #Author dev@Borodin-Atamanov.ru
 #License: MIT
 
-install_system chromium-browser
+install_system firefox-esr
 install_system rsync
 install_system git
 
 
-if [ -f "${chromium_config_dir}/dont_overwrite_config.flag" ]; then
-  echo "${chromium_config_dir}/dont_overwrite_config.flag exists! So don't overwrite configuration."
+if [ -f "${firefox_config_dir}/dont_overwrite_config.flag" ]; then
+  echo "${firefox_config_dir}/dont_overwrite_config.flag exists! So don't overwrite configuration."
   exit 0
 fi
 
-temp_dir="${TMPDIR:-/tmp}/dzintara_chromium_dir-$(date "+%F-%H-%M-%S")";
+temp_dir="${TMPDIR:-/tmp}/temp_dir-$(date "+%F-%H-%M-%S")";
 mkdir -pv "${temp_dir}";
 echo "clone config from github";
 slog "<7>$(show_var work_dir)";
-git clone --depth 1 "${chromium_config_github_url}" "${temp_dir}";
+git clone --depth 1 "${firefox_config_github_url}" "${temp_dir}";
 cd "${temp_dir}";
 
-rsync --recursive --update --mkpath --copy-links --executability  --sparse --whole-file --delete-after --ignore-errors --exclude='.git' --exclude='.git*' --human-readable --stats --itemize-changes "${temp_dir}/" "${chromium_config_dir}/"  | tr -d '\n'
+rsync --recursive --update --mkpath --copy-links --executability  --sparse --whole-file --delete-after --ignore-errors --exclude='.git' --exclude='.git*' --human-readable --stats --itemize-changes "${temp_dir}/" "${firefox_config_dir}/"  | tr -d '\n'
 
-echo 'this file created by Dzintara chromium_setup.sh' > "${chromium_config_dir}/dont_overwrite_config.flag";
+echo 'this file created by Dzintara script' > "${firefox_config_dir}/dont_overwrite_config.flag";
 find "${chromium_config_dir}/" -type d -exec chmod -v 0777 {} \;
 find "${chromium_config_dir}/" -type f -exec chmod -v 0640 {} \;
-chown  --changes --recursive  i:i "${chromium_config_dir}";
+chown  --changes --recursive  i:i "${firefox_config_dir}";
 
 exit
