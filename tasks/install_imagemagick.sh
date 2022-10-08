24d073348d494a5b68ebf00a30003b028c126f29
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2022-10-09 01:21:21 +0400

    okomou-1008-2210090121

--- tasks/install_imagemagick.sh
+++ tasks/install_imagemagick.sh
@@ -14,20 +14,48 @@ show_var os_codename
 show_var architecture
 
 # https://imagemagick.org/script/security-policy.php
-write_config "usr:local:etc:ImageMagick-7:policy.xml"
+write_config "usr:local:etc:ImageMagick-7:policy.xml" '/usr/local/etc/ImageMagick-6/policy.xml'
+
+# https://askubuntu.com/questions/1042436/how-to-install-delegate-libraries-for-image-magick-7-0-7
+
 
 if [ -f "/usr/local/bin/magick" ]; then
   echo "app already exists in system. Exit"
   exit 0
 fi
 
+
+
+app_install_dir="/usr/local/bin/"
+
+
+# TODO if x86_64 download AppImage https://imagemagick.org/archive/binaries/magick
+
+if [[ "$architecture" == 'amd64']]; then
+  :
+  # download appimage
+  download_url='https://imagemagick.org/archive/binaries/magick'
+  wget --output-document="${app_install_dir}/magick" --retry-on-host-error --no-check-certificate "$download_url"
+  chmod -v 0444 {} "${app_install_dir}/magick"
+  # chown  --changes --recursive  root:root  "${app_install_dir}/magick"
+else
+  :
+  # download from repository
+  install_system imagemagick
+fi;
+
+
+exit
+
+# compile from source code
 install_system build-essential
+
 
 download_url='https://github.com/ImageMagick/ImageMagick.git'
 
 #temp_file="/tmp/archive.tar.gz"
 temp_dir="$(mktemp -d -t dzintara-XXXXX)"
-app_install_dir="/usr/local/bin/"
+
 #rm -v "$temp_file"
 #wget --output-document="$temp_file" --retry-on-host-error --no-check-certificate "$download_url"
 echo "clone from github";
@@ -39,12 +67,16 @@ if $exit_code ; then
   echo "error! exit_code after git clone is $exit_code "
   exit $exit_code
 else
+  # https://imagemagick.org/script/advanced-linux-installation.php
+  # https://askubuntu.com/questions/1042436/how-to-install-delegate-libraries-for-image-magick-7-0-7
   ./configure
   make
   ./configure --with-modules
   make install
   ldconfig /usr/local/lib
   make check
+  # BUG: after this compilation imagemagick can not read simpe png file (it needs delegates for every file format)
+
   # extract tar to temp directory
   # rm -rvf "$temp_dir"
   # mkdir -pv "$temp_dir"
